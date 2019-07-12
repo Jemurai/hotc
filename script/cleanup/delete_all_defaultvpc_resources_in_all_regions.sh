18e93187d268c1aa2b918b88a3c59599d56fdcd7
Joseph Kerby <joe@jemurai.com>
2019-07-11 22:09:03 -0500

    Additional resources

--- script/cleanup/delete_all_defaultvpc_resources_in_all_regions.sh
+++ script/cleanup/delete_all_defaultvpc_resources_in_all_regions.sh
@@ -3,5 +3,5 @@ DIR=$(dirname $0)
 
 for region in $(aws ec2 describe-regions --query "Regions[].RegionName" --output text)
 do
-    ${DIR}/delete_all_vpc_resources_in_region.sh ${region}
+    ${DIR}/delete_all_defaultvpc_resources_in_region.sh ${region}
 done
