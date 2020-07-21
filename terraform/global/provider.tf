350283a94bf2ddd5f0d947ef11fb933581761d45
Joseph Kerby <work@joekerby.net>
2020-07-21 14:06:24 -0500

    Upgrade terraform to 0.12 format.

--- terraform/global/provider.tf
+++ terraform/global/provider.tf
@@ -1,11 +1,13 @@
 provider "aws" {
-  region = "${var.aws_region}"
+  region = var.aws_region
 }
 
 terraform {
+  // bucket = "<s3_bucket_prefix>tfstate"
   // bucket = "<s3_bucket_prefix>tfstate"
   backend "s3" {
     key    = "global/terraform.tfstate"
     region = "us-east-2"
   }
 }
+
